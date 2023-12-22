dba201006b21655aca38089d5a5eeb7eeca1f864
TerryHall-Git <terryghall1981@gmail.com>
2023-12-22 05:16:45 -0600

    updated nav css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,79 +12,74 @@ export default function Navbar() {
   const [selection, setSelection] = useState("");
   const navigate = useNavigate();
 
-  const navDeselected =
-    "flex items-center px-6 py-2 mt-2 text-highlight3 hover:bg-secondary hover:bg-opacity-25 hover:text-white";
-  const navSelected =
-    "flex items-center px-6 py-2 mt-2 text-white bg-opacity-25 bg-highlight1";
-
   function homeClickHandler() {
     setSelection("");
     navigate("/");
   }
 
   return (
-    <>
-      <div className="Navbar-header" onClick={homeClickHandler}>
-        <div className="p-3 inline-block w-16">
+    <div>
+      <div className="Navbar-top" onClick={homeClickHandler}>
+        <div className="Navbar-homeIcon">
           <DeepThoughtsIcon />
         </div>
-        <div className="p-3 inline-block w-60 text-2xl mt-1">
+        <div className="Navbar-homeTitle">
           <span>Deep Thoughts</span>
         </div>
       </div>
-      <div className="absolute top-16 left-0 w-72 m-0 bg-primary border-r-4 border-secondary min-h-full">
-        <nav className="mt-0 p-0 h-full">
+      <div className="Navbar-left">
+        <nav>
           <Link
             to="/projects"
             name="projects"
-            className={selection === "projects" ? navSelected : navDeselected}
+            className={selection === "projects" ? "Navbar-selected" : "Navbar-deselected"}
             onClick={(e) => setSelection(e.target.closest("a").name)}
           >
             <ProjectsIcon />
-            <span className="mx-3">Projects</span>
+            <span className="Navbar-icon">Projects</span>
           </Link>
 
           <Link
             to="/overview"
             name="overview"
-            className={selection === "overview" ? navSelected : navDeselected}
+            className={selection === "overview" ? "Navbar-selected" : "Navbar-deselected"}
             onClick={(e) => setSelection(e.target.closest("a").name)}
           >
             <OverviewIcon />
-            <span className="mx-3">Overview</span>
+            <span className="Navbar-icon">Overview</span>
           </Link>
 
           <Link
             to="/boards"
             name="boards"
-            className={selection === "boards" ? navSelected : navDeselected}
+            className={selection === "boards" ? "Navbar-selected" : "Navbar-deselected"}
             onClick={(e) => setSelection(e.target.closest("a").name)}
           >
             <BoardsIcon />
-            <span className="mx-3">Boards</span>
+            <span className="Navbar-icon">Boards</span>
           </Link>
 
           <Link
             to="/tasks"
             name="tasks"
-            className={selection === "tasks" ? navSelected : navDeselected}
+            className={selection === "tasks" ? "Navbar-selected" : "Navbar-deselected"}
             onClick={(e) => setSelection(e.target.closest("a").name)}
           >
             <TasksIcon />
-            <span className="mx-3">Tasks</span>
+            <span className="Navbar-icon">Tasks</span>
           </Link>
 
           <Link
             to="/admin"
             name="admin"
-            className={selection === "admin" ? navSelected : navDeselected}
+            className={selection === "admin" ? "Navbar-selected" : "Navbar-deselected"}
             onClick={(e) => setSelection(e.target.closest("a").name)}
           >
             <AdministrationIcon />
-            <span className="mx-3">Administration</span>
+            <span className="Navbar-icon">Administration</span>
           </Link>
         </nav>
       </div>
-    </>
+    </div>
   );
 }
