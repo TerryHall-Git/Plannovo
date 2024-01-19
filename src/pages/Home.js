20fde67ca9147e90608c4ae3b41e252e8db0fa7c
TerryHall-Git <terryghall1981@gmail.com>
2024-01-19 16:57:38 -0600

    updated welcome page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,9 +8,7 @@ const Home = () => {
     <div className="Home">
       <div className="Home-content">
         <h1>Welcome!</h1>
-        <p>
-          To get started, click on the "Projects" tab to begin a new project.
-        </p>
+        <p>Click on the "Projects" tab to begin a new project.</p>
       </div>
     </div>
   );
