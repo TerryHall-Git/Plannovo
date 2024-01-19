259a0caee321de1ee8d0ff4a627c6844c42c2b3f
TerryHall-Git <terryghall1981@gmail.com>
2024-01-19 08:43:46 -0600

    fix: cl

--- src/components/Board.js
+++ src/components/Board.js
@@ -34,7 +34,6 @@ export default function Board() {
   const interacted = useRef(false);
   const [showTaskList, setShowTaskList] = useState(false);
 
-  console.log("updating");
   function refresh() {
     setContainers(projMgr.getActiveContainers());
   }
